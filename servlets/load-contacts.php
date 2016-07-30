9a8aac10d243eb1dc1e10566d8ccae38bfb76719
olimpia marin <olimpiamarin84@gmail.com>
2016-07-30 16:38:24 +0300

    creare functii getConnection si getContacts

--- servlets/load-contacts.php
+++ servlets/load-contacts.php
@@ -1,8 +1,8 @@
-<?php include ("function.php");
-$conn = pg_connect("host=54.93.65.5 port=5432 dbname=4_Olimpia user=fasttrackit_dev password=fasttrackit_dev");
+<?php
+include("functions.php");
 
+$conn = getConnection();
 $records = getContacts($conn);
-
 pg_close($conn);
 
 echo json_encode($records);
--- servlets/remove-contact.php
+++ servlets/remove-contact.php
@@ -1,20 +1,11 @@
 <?php
-$conn = pg_connect("host=54.93.65.5 port=5432 dbname=4_Olimpia user=fasttrackit_dev password=fasttrackit_dev");
+include("functions.php");
 
+$conn = getConnection();
 $id = $_POST["id"];
 $resultDel = pg_query($conn, "delete from agenda where id=" .$id);
 
-$result = pg_query($conn, "select * from agenda");
-while($row = pg_fetch_array($result)){
-    $person = array(
-        "id"=>$row["id"],
-        "firstName" => $row["nume"],
-        "lastName" => $row["prenume"],
-        "telefon" => $row["telefon"]
-    );
-    $records[] = $person;
-}
-
+$records = getContacts($conn);
 pg_close($conn);
 
 echo json_encode($records);
